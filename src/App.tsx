73247ba2be339d53915a26d858d76a10e82b7ea4
patrperr <patrice.perraudin@edu.vs.ch>
2022-10-12 15:58:47 +0200

    12.10.2022

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import CVmenu from './CVmenu';
 import CVDisplayer from './cvdisplayer/CVDisplayer';
+import CreateItems from './cvForm/CreateItems';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <CVmenu/>
       <CVDisplayer/>
+      
     </div>
   ); 
 }
