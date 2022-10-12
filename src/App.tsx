82178e8a64a835cd8a489807567b93c257151473
lamacrym0 <lamastico2.0@gmail.com>
2022-10-12 05:55:35 -0700

    je suis con 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import CVmenu from './CVmenu';
 import CVDisplayer from './cvdisplayer/CVDisplayer';
+import CreateItems from './cvForm/CreateItems';
 
 function App() {
-
-
 
   return (
     <div className="App">
       <CVmenu/>
       <CVDisplayer/>
+      <CreateItems/>
     </div>
   ); 
 }
