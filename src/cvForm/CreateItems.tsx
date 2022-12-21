d6d350d94883405727be9ec975deef4c81ed4523
lamacrym0 <lamastico2.0@gmail.com>
2022-12-21 06:49:13 -0800

    liste selection type champ début

--- src/cvForm/CreateItems.tsx
+++ src/cvForm/CreateItems.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import "./styles/createItems.css"
 import Button from '@mui/material/Button';
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
@@ -6,22 +6,39 @@ import AddIcon from '@mui/icons-material/Add';
 import './../cvRessources/cvs/sampleCVJSON.json';
 import { nodeModuleNameResolver } from 'typescript';
 import ListeSelectorType from './ListeSelectorType'
+import CVSection from './CVSection';
 
-export class 
-    CreateItems
- extends Component {
-  render() {
-    return (
-      <div className='create-items'>
-        <Button className='create-items-add-button' variant="outlined" onClick={() => {
-          console.log('clicked')
-          }} endIcon={<AddIcon />}>
-          Create item
-        </Button>
-      </div>
-    )
+
+export default function CreateItems() {
+   const [items,setItems] = useState<JSX.Element[]>([])
+  
+  function addComponent() { 
+    let newItems = [...items];
+    newItems.push(<ListeSelectorType onChange={onEachClick} delete={onEachClick} id={newItems.length} />);
+    setItems(newItems)
+    
   }
+  let onEachClick = (id:any) =>{
+    console.log(id)
+  }
+
+   return (
+    <div className='create-items'>
+      <div>
+        {
+          items.map((data:JSX.Element) => {
+            return <div>{data}</div>
+          })
+        }
+      </div>
+      <Button  className='create-items-add-button' variant="outlined" onClick={
+        addComponent
+      } endIcon={<AddIcon />}>
+        Create item
+      </Button>
+    </div>
+  )
+
 }
 
-export default 
-    CreateItems
+
--- src/cvForm/ListeSelectorType.tsx
+++ src/cvForm/ListeSelectorType.tsx
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
-export class ListeSelectorType extends Component {
-  render() {
-    return (
-      <div>ListeSelectorType</div>
-    )
-  }
+export default function  ListeSelectorType(props:{onChange:(id:any)  => void, delete:(id:any)  => void,id:any}) {
+  const[id,setId] = useState(props.id)
+  
+  return (
+    <div>  
+      <select onChange={()=>props.onChange(props.id)} name="pets" id="pet-select">
+        <option value="">--Please choose an option--</option>
+        <option value="Title">Title</option>
+        <option value="Textarea">Textarea</option>
+      </select>
+      <button onClick={() => props.delete(props.id)}>X</button>
+    </div>
+  )
 }
-
-export default ListeSelectorType
