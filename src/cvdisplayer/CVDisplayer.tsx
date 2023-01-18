1f5cdaedabefa6a0aa126c64c0da0acb9f87bcd3
patrperr <patrice.perraudin@edu.vs.ch>
2023-01-18 16:00:07 +0100

    Json uploader is active ! :)

--- src/cvdisplayer/CVDisplayer.tsx
+++ src/cvdisplayer/CVDisplayer.tsx
@@ -6,9 +6,9 @@ import { Button, Input } from '@mui/material';
 import { FileUploadOutlined } from '@mui/icons-material';
 
 
-export class CVDisplayer extends Component<{}, { 
+export class CVDisplayer extends Component<{}, {
   cvToDisplay: CVObject,
-  fileReference?: File 
+  fileReference?: File
 
 }> {
 
@@ -29,18 +29,35 @@ export class CVDisplayer extends Component<{}, {
     this.setState({ cvToDisplay: cvToDisplayNoState });
   }
 
-  UploadNewCV(){
-    console.log(this.state.fileReference)
+  UploadNewCV() {
+    // console.log("UploadNewCV"); //!
+    if (this.state.fileReference !== undefined) {
+      // console.log(this.state.fileReference); //!
+      const fileReader = new FileReader();
+      let newCV: CVObject;
+
+      fileReader.readAsText(this.state.fileReference, "UTF-8");
+      fileReader.onload = e => {
+        const target = e.target;
+        const result: string | undefined = target?.result?.toString();
+        // console.log(result);//!
+        if (result !== undefined) {
+          newCV = JSON.parse(result);
+        }
+        this.setState({ cvToDisplay: newCV });
+      };
+
+      // console.log(this.state.cvToDisplay);//!
+
+    } else {
+      alert("Error while uploading CV (alfa alert)")
+    }
   }
 
 
   SelectFileToUpload = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
-      this.setState({fileReference: e.target.files[0]});
-      console.log("e.target.files[0]");
-      console.log(e.target.files[0]);
-      console.log("this.state.fileReference");
-      console.log(this.state.fileReference);
+      this.setState({ fileReference: e.target.files[0] });
     }
   }
 
@@ -48,7 +65,7 @@ export class CVDisplayer extends Component<{}, {
 
     let baseKey: string = "CvMainSection";
     let keynumber: number = 0;
-    let uplaodisActive : boolean = false;
+    let uplaodisActive: boolean = false;
 
     if (this.state.fileReference !== undefined) {
       uplaodisActive = true;
@@ -57,14 +74,14 @@ export class CVDisplayer extends Component<{}, {
     return (
       <div>
         <div>
-        <Input type="file" onChange={this.SelectFileToUpload} /> <br/>
-        <Button variant='contained' startIcon={<FileUploadOutlined />} onClick={this.UploadNewCV} disabled={!uplaodisActive}>Upload CV</Button>
+          <Input type="file" onChange={this.SelectFileToUpload} /> <br />
+          <Button variant='contained' startIcon={<FileUploadOutlined />} onClick={this.UploadNewCV.bind(this)} disabled={!uplaodisActive}>Upload CV</Button>
         </div>
         {
           this.state.cvToDisplay.allSections?.map(section => {
             keynumber++;
             let key = baseKey + keynumber;
-
+            // console.log(this.state.cvToDisplay);//!
             return (<div key={key}>
               <CPSection sectionToDiplay={section} isChild={false} />
             </div>)
