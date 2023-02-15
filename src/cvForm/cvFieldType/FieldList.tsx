13e730c10a6b111f8af079455230c64f17b0f9ee
lamacrym0 <lamastico2.0@gmail.com>
2023-02-15 06:41:28 -0800

    create field type

---
+++ src/cvForm/cvFieldType/FieldList.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+export default function FieldList() {
+    const [statePrewiew, setStatePrewiew] = useState(false);
+    const [stateText, setStateText] = useState('');
+    let result
+    if(!statePrewiew){
+        result = <div>
+            <input type="textarea" value={stateText} onChange={e=>{setStateText(e.target.value)}} onBlur={e=>{setStateText(e.target.value);setStatePrewiew(true)}} />
+        </div>
+    }
+    else{
+        if(stateText === '')
+        {
+            return null
+        }
+        result = <div style={{width: "auto",margin:"0px auto"}} onClick={()=>{setStatePrewiew(false)}}>
+            {stateText}
+        </div>
+    }
+    return result
+}
---
+++ src/cvForm/cvFieldType/FieldTitle.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+export default function FieldTitle() {
+    const [statePrewiew, setStatePrewiew] = useState(false);
+    const [stateText, setStateText] = useState('');
+    let result
+    if(!statePrewiew){
+        result = <div>
+            <input type="textarea" value={stateText} onChange={e=>{setStateText(e.target.value)}} onBlur={e=>{setStateText(e.target.value);setStatePrewiew(true)}} />
+        </div>
+    }
+    else{
+        if(stateText === '')
+        {
+            return null
+        }
+        result = <h1 style={{width: "auto",margin:"0px auto"}} onClick={()=>{setStatePrewiew(false)}}>
+            {stateText}
+        </h1>
+    }
+    return result
+}
